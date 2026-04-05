fe36377f7564be172bb8c706dba6f037cffed25e
gethin101 <gethingrice@gmail.com>
2026-04-05 18:58:54 +0100

    Added wallet for user, labelled functions and switched to ("-" * 50) instead of 50 lines

--- Cinema/gethin.py
+++ Cinema/gethin.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 customer_LoggedIn = False
 admin_LoggedIn = False
 login_attempt = 0
+CINEMA_NAME = "Gethin-Plex"
 
 
 admin_login = {
@@ -13,8 +14,10 @@ admin_login = {
 
 user_login = {
     "username": "gethin",
-    "password": "login"
+    "password": "login",
+    "wallet": 50.00
 }
+
 
 @dataclass
 class Film:
@@ -23,32 +26,34 @@ class Film:
     category: str
     tickets: int
 
-spiderman = Film("Spiderman", 10.99, "Action", 120)
-starwars = Film("Star Wars", 8.50, "Science Fiction", 95)
-jurassic_park = Film("Jurassic Park", 7.25, "Adventure", 70)
+spiderman = Film("Spiderman", 10.99, "Action", 20)
+starwars = Film("Star Wars", 8.50, "Science Fiction", 18)
+jurassic_park = Film("Jurassic Park", 7.25, "Adventure", 15)
 
 films = [spiderman, starwars, jurassic_park]
 
 
+#quits program
 def rejection():
     print("\nEdit movie confirmation rejected")
     time.sleep(1)
     quit()
 
-
+#prints all movies info out
 def display_movies():
     print("\n     CURRENT MOVIES")
-    print("--------------------------------------------------")
+    print("-" * 50)
     for film in films:
         print(f"- {film.movie}")
         print(f"    Category : {film.category}")
         print(f"    Price    : £{film.price:.2f}")
         print(f"    Tickets  : {film.tickets}")
-        print("--------------------------------------------------")
+        print("-" * 50)
 
 
 # ================== Login ==================
 
+#login function
 def login():
     global admin_LoggedIn, customer_LoggedIn, login_attempt
 
@@ -56,9 +61,9 @@ def login():
 
     #admin login
     if login_name == admin_login["username"]:
-        print("------------------------------------------------------------------")
+        print("-" * 66)
         confirm_admin = input("Are you sure you want to access ADMIN PANEL? (y/n) ").lower()
-        print("------------------------------------------------------------------")
+        print("-" * 66)
 
         if confirm_admin == "y":
             password = input("Enter password ").lower()
@@ -70,9 +75,9 @@ def login():
             else:
                 while login_attempt < 3:
                     login_attempt += 1
-                    print("\n------------------------------------------------")
+                    print("\n-" * 48)
                     print(f"Incorrect password. Attempt: {login_attempt}")
-                    print("------------------------------------------------")
+                    print("-" * 48)
                     time.sleep(2)
 
                     if login_attempt < 3:
@@ -98,18 +103,24 @@ def login():
         confirm_login = input(f"\nAre you sure you want to log in as {login_name}? (y/n) ").lower()
 
         if confirm_login == "y":
+            print("-" * 50)
             password = input("Enter password: ")
 
             if password == user_login["password"]:
                 customer_LoggedIn = True
                 print(f"\nCustomer logged in as {login_name}")
+                print()
+                print("=" * 25)
+                print(f"Current balance: £{user_login['wallet']:.2f}")
+                print("=" * 25)
+                time.sleep(2)
 
             else:
                 while login_attempt < 3:
                     login_attempt += 1
-                    print("\n------------------------------------------------")
+                    print("\n-" * 48)
                     print(f"Incorrect password. Attempt: {login_attempt}")
-                    print("------------------------------------------------")
+                    print("-" * 48)
                     time.sleep(2)
 
                     if login_attempt < 3:
@@ -130,20 +141,20 @@ def login():
             quit()
 
     else:
-        print("\nIncorrect username..")
+        print("\nThis account doesn't exist ")
         time.sleep(1)
         quit()
 
 login()
 
 
-#customer logged in
+#-------Customer logged in--------
 if customer_LoggedIn:
-    print("----------------------------------------------------------------------------------")
-    print("Welcome to Gethin-Plex, which movie would you like to see?")
+    print("-" * 82)
+    print(f"Welcome to {CINEMA_NAME}, which movie would you like to see?")
     display_movies()
 
-    book_film = input().lower()
+    book_film = str(input("Enter movie: ")).lower()
     selected_film = None
 
     for film in films:
@@ -171,13 +182,13 @@ if customer_LoggedIn:
 
         else:
             selected_film.tickets -= amount
-            print("------------------------------------------------------------")
+            print("-" * 60)
             print(f"You just booked {amount} tickets for {selected_film.movie}!")
-            print("------------------------------------------------------------")
+            print("-" * 60)
             print(f"Tickets left: {selected_film.tickets}")
 
 
-#admin logged in
+#-------Admin logged in--------
 elif admin_LoggedIn:
     display_movies()
 
@@ -201,9 +212,9 @@ elif admin_LoggedIn:
         section_edit = input(f"Which data would you like to edit for {selected_movie.movie}?   Price | Tickets | Category  ").lower()
 
         if section_edit == "price":
-            print("\n-----------------------------------------------------")
+            print("\n-" * 53)
             selected_movie.price = float(input(f"Current price: £{selected_movie.price:.2f} | Enter new price: £"))
-            print("-----------------------------------------------------")
+            print("-" * 53)
             print(f"{selected_movie.movie} price updated to £{selected_movie.price:.2f}!")
             print()
             time.sleep(1)
